42d326cb61c7003fced717b46688eb446b5278d0
Caleb Fletcher <caleb@fletcher.cf>
2021-08-10 23:18:58 +1000

    Array_lookup: Add implementation

--- array_lookup/src/main.rs
+++ array_lookup/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    let array = [1, 1, 2, 3, 5, 8, 13, 21, 34];
+
+    println!("Please enter a value: ");
+
+    // Read integer from stdin
+    let mut index = String::new();
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("Unable to read line from stdin.");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Unable to parse integer.");
+    
+    if index >= array.len() {
+        println!("Value should be less than {}", array.len());
+        return
+    }
+
+    println!("Value at index {} is {}", index, array[index]);
 }
