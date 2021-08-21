af3938a952832cdf59df6c5980e2cd39868d933f
Caleb Fletcher <caleb@fletcher.cf>
2021-08-21 22:58:30 +1000

    feat(is_palindrome): add palindrome checker

--- is_palindrome/src/main.rs
+++ is_palindrome/src/main.rs
@@ -1,8 +1,22 @@
+fn is_palindrome(buf: &str) -> bool {
+    let chars: Vec<char> = buf.trim().chars().collect();
+
+    let strlen = chars.len();
+    let midway = (strlen + 1) / 2;
+
+    for i in 0..midway {
+        if chars[i] != chars[strlen-i-1] {
+            return false;
+        }
+    }
+    return true;
+}
 
 fn main() {
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).expect("Unable to read input.");
     let buf = buf.trim();
 
-
+    let result = is_palindrome(buf);
+    println!("Is a palindrome? {}", if result {"yes"} else {"no"});
 }
