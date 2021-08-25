0f6ada908ba9b39e81d8c4e08ff703b7c1e39e0c
Caleb Fletcher <caleb@fletcher.cf>
2021-08-25 22:43:27 +1000

    feat(minigrep): add file reading

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,11 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let query = &args[1];
     let filename = &args[2];
+
+    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
 }
