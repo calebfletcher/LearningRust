0ede61cf1fdaa556ae4caea686b32fb6a392e368
Caleb Fletcher <caleb@fletcher.cf>
2021-08-25 23:20:51 +1000

    refactor(minigrep): split core code into lib file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    let query = &args[1];
-    let filename = &args[2];
 
-    let content = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
