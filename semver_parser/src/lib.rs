3b7a85def3cb659e3a6fe55a6f6f5dac2339f49c
Caleb Fletcher <caleb@fletcher.cf>
2021-08-26 22:04:03 +1000

    feat(semver_parser): add SemVer struct fields

--- semver_parser/src/lib.rs
+++ semver_parser/src/lib.rs
@@ -1,10 +1,21 @@
-pub struct SemVer {
+pub struct SemVer<'a> {
+    major: u32,
+    minor: u32,
+    patch: u32,
+    prerelease: Vec<&'a str>,
+    build_metadata: Vec<&'a str>,
+    is_development: bool,
 }
 
-impl SemVer {
+impl<'a> SemVer<'a> {
     pub fn new(value: &str) -> Result<SemVer, &str> {
         let sv = SemVer {
-
+            major: 0,
+            minor: 0,
+            patch: 0,
+            prerelease: vec![],
+            build_metadata: vec![],
+            is_development: false
         };
 
         Ok(sv)
