681b9c13fda1771d81f4232009172b1398df5fca
Caleb Fletcher <caleb@fletcher.cf>
2021-08-26 23:25:22 +1000

    feat(semver_parser) parse version parts

--- semver_parser/src/lib.rs
+++ semver_parser/src/lib.rs
@@ -15,15 +15,33 @@ impl<'a> SemVer<'a> {
         let prerelease_split: Vec<&str> = version_without_build.split('-').collect();
         let (version_without_prerelease, prerelease) = prerelease_split.split_first().unwrap();
 
+        let version = SemVer::parse_version(version_without_prerelease)?;
+
         let sv = SemVer {
-            major: 0,
-            minor: 0,
-            patch: 0,
+            major: version[0],
+            minor: version[1],
+            patch: version[2],
             prerelease: vec![],
             build_metadata: vec![],
             is_development: false
         };
 
         Ok(sv)
+    }
+
+    fn parse_version(version: &str) -> Result<Vec<u32>, &str> {
+        let version_split: Vec<u32> = version
+            .split('.')
+            .filter(|part| part.chars().count() > 0)
+            .filter(|part| part.chars().all(char::is_numeric))
+            .filter(|part| part.chars().next().unwrap() != '0')
+            .map(|part| part.parse::<u32>().unwrap())
+            .collect();
+
+        if version_split.len() != 3 {
+            return Err("incorrect number of version parts, expected 3");
+        }
+
+        Ok(version_split)
     }
 }
