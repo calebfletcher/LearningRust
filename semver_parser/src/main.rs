4004fa34bd85e973f3d31798d5bf2451fbc9fae3
Caleb Fletcher <caleb@fletcher.cf>
2021-08-26 21:24:47 +1000

    feat(semver_parser): add cmdline parsing

--- semver_parser/src/main.rs
+++ semver_parser/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process;
+use semver_parser::SemVer;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Not enoough arguments!");
+        eprintln!("Usage: {} VERSION", args[0]);
+        process::exit(1);
+    }
+
+    let sv = SemVer::new(&args[1]).unwrap_or_else(|err| {
+        eprintln!("Unable to parse semver: {}", err);
+        process::exit(1);
+    });
 }
