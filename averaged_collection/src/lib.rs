a4f1c2f46a73ffb8c8e6d24fe1f9611f5b8d2803
Caleb Fletcher <caleb@fletcher.cf>
2021-09-08 23:06:51 +1000

    feat(averaged_collection): add function definitions

---
+++ averaged_collection/src/lib.rs
@@ -0,0 +1,18 @@
+pub struct AveragedCollection<T> {
+    data: Vec<T>,
+    average: T,
+}
+
+impl<T> AveragedCollection<T> {
+    pub fn add(&mut self) {
+
+    }
+
+    pub fn remove(&mut self) {
+
+    }
+
+    fn update_average(&mut self) {
+
+    }
+}
