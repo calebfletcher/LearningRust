e9887cba75bdea7b56b9dde2471cfd416f92168d
Caleb Fletcher <caleb@fletcher.cf>
2021-09-09 00:23:20 +1000

    feat(averaged_collection): add binary for testing

--- averaged_collection/src/main.rs
+++ averaged_collection/src/main.rs
@@ -1,5 +1,11 @@
-
+use averaged_collection::AveragedCollection;
 
 fn main() {
-    println!("Hello, world!");
+    let mut collection = AveragedCollection::new();
+
+    collection.push(7);
+    collection.push(2);
+    collection.push(10);
+
+    println!("Average {}", collection.average());
 }
