a6d7743163ec54f56ace738a7d30c73e7649b22e
Caleb Fletcher <caleb@fletcher.cf>
2021-10-06 01:36:39 +1100

    feat(web_server): add TCP server

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8000").expect("Unable to bind to port");
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection Established");
+    }
 }
