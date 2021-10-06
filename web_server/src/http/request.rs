f74d183538c168101b522fe402dcd63456c951b3
Caleb Fletcher <caleb@fletcher.cf>
2021-10-06 20:55:55 +1100

    feat(web_server): add request struct

--- web_server/src/http/request.rs
+++ web_server/src/http/request.rs
@@ -1,3 +1,6 @@
+use crate::http::cookies;
+use crate::http::headers;
+
 #[derive(Debug)]
 enum Method {
     GET,
@@ -9,17 +12,38 @@ pub struct Request {
     method: Method,
     target: String,
     version: String,
+    headers: headers::Headers,
+    cookies: Option<cookies::Cookies>,
+    body: String,
 }
 
 impl Request {
-    pub fn new(request_str: &str) -> Result<Request, &str> {
-        let request_split: Vec<&str> = request_str.split_whitespace().collect();
+    pub fn new(request: &str) -> Result<Request, &str> {
+        // Split request
+        let (request_str, remainder) = request
+            .split_once("\r\n")
+            .expect(r"Request did not contain a \r\n");
+        let (headers, body) = remainder
+            .split_once("\r\n\r\n")
+            .expect("Unable to split headers and body form request");
 
+        // Parse headers
+        let headers = headers::Headers::new(headers);
+        let cookies = match headers.get("Cookie") {
+            Some(cookies_raw) => cookies::Cookies::new(cookies_raw),
+            None => None,
+        };
+
+        // Parse request line
+        let request_split: Vec<&str> = request_str.split_whitespace().collect();
         if let [method, target, version] = request_split[..] {
             Ok(Request {
                 method: Request::parse_method(method)?,
                 target: String::from(target),
                 version: String::from(version),
+                headers,
+                cookies,
+                body: String::from(body),
             })
         } else {
             Err("Request does not contain 3 components")
--- web_server/src/http/server.rs
+++ web_server/src/http/server.rs
@@ -2,8 +2,6 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-use crate::http::cookies;
-use crate::http::headers;
 use crate::http::request;
 
 pub struct Server {
@@ -28,28 +26,14 @@ impl Server {
     fn handle_connection(&self, mut stream: TcpStream) {
         // Read raw request
         let mut buf = [0; 1024];
-        stream.read(&mut buf).expect("Unable to read from stream");
-        let request = String::from_utf8_lossy(&buf);
+        let bytes_read = stream.read(&mut buf).expect("Unable to read from stream");
+        let raw_request = String::from_utf8_lossy(&buf[..bytes_read]);
 
         // Parse request
-        let (request, remainder) = request
-            .split_once("\r\n")
-            .expect(r"Request did not contain a \r\n");
-        let request = request::Request::new(request).expect("Unable to parse request");
-        let (headers, body) = remainder
-            .split_once("\r\n\r\n")
-            .expect("Unable to split headers and body form request");
-        let headers = headers::Headers::new(headers);
-        let cookies = match headers.get("Cookie") {
-            Some(cookies_raw) => cookies::Cookies::new(cookies_raw),
-            None => None,
-        };
+        let request = request::Request::new(&raw_request);
 
         // Debug request
         println!("{:#?}", request);
-        println!("{:#?}", headers);
-        println!("{:#?}", cookies);
-        println!("{}", body);
 
         // Send response
         let body = fs::read_to_string("hello.html").expect("Unable to read response file");
