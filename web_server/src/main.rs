303a71c07ea1a3bfcc9230ea7be4a1d11e5541ea
Caleb Fletcher <caleb@fletcher.cf>
2021-10-06 16:26:33 +1100

    feat(web_server): read raw request

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,9 +1,20 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("localhost:8000").expect("Unable to bind to port");
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
         println!("Connection Established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).expect("Unable to read from stream");
+
+    let buf = String::from_utf8_lossy(&buf);
+
+    println!("Request: {}", buf);
 }
